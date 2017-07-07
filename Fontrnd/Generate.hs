4b92f53f850f1ae588fbf84a78417ebef89f8cef
siers <wimuan@gmail.com>
2017-07-07 11:37:22 +0300

    Filter non-printable characters.

--- Fontrnd/Generate.hs
+++ Fontrnd/Generate.hs
@@ -4,6 +4,6 @@ import System.Random
 import Data.Char
 
 generate :: StdGen -> [Integer] -> String
-generate rnd list = map chr $ take 320 indexes
+generate rnd list = filter isPrint . map chr $ take 320 indexes
     where
         indexes = randomRs (0, (length list) - 1) rnd
