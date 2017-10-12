1e8efff26ab85da6b99b970b8e7686168d616607
siers <wimuan@gmail.com>
2017-10-12 12:52:36 +0300

    Allow changing charset computation choices + README.

--- fontrnd.hs
+++ fontrnd.hs
@@ -1,11 +1,34 @@
-import Data.List
-import Fontrnd.Parser
+import Data.List (union, intersect)
+import Data.Map (Map, keys, fromList, lookup)
+import Data.Maybe
 import Fontrnd.Generate
+import Fontrnd.Parser
 import System.Environment
 import System.Random
+import Prelude hiding (lookup)
+
+choices :: Map String ([Integer] -> [Integer] -> [Integer])
+choices = fromList
+    [ ("union", union)
+    , ("or", union)
+    , ("and", intersect)
+    , ("intersect", intersect)
+    ]
+
+usage :: String
+usage =
+    "usage: ./fontrnd <algos> <fonts>+\n" ++
+    "  possible algos: " ++ show (keys choices)
 
 main = do
-    glyphs <- mapM glyphs =<< getArgs
+    (algo:fonts) <- getArgs
+
+    glyphs <- mapM glyphs fonts
     rnd <- newStdGen
 
-    putStrLn . generate rnd $ foldr1 intersect glyphs
+    let zipper = algo `lookup` choices
+
+    maybe
+        (putStrLn usage)
+        (putStrLn . generate rnd . flip foldr1 glyphs)
+        zipper
